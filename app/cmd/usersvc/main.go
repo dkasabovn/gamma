7b9c8a9eda07e9365bce7797c40b57abf30a7126
Gabriel <gabrieldiaziv@gmail.com>
2021-10-20 10:33:30 -0500

    added jwt auth and two basic endpoints to test

--- app/cmd/usersvc/main.go
+++ app/cmd/usersvc/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"gamma/app/system/auth"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+
+
 func main() {
 
+	e := echo.New()
+
+
+	authRequired := e.Group("/api")
+
+	// Configure middleware with the custom claims type
+	jwtConfig := middleware.JWTConfig{
+		Claims:     &auth.Claims{},
+		SigningKey: []byte(auth.GetJwtSecret()),
+		TokenLookup: "cookie:access-token",
+	}
+	authRequired.Use(middleware.JWTWithConfig(jwtConfig))
+	authRequired.GET("", authNeed)
+
+	e.GET("/", noAuth)
+
+	e.Logger.Fatal(e.Start(":8000"))
+
+
+}
+
+func noAuth(ctx echo.Context) error {
+	return ctx.JSON(http.StatusAccepted, "No auth needed")
+}
+
+func authNeed(ctx echo.Context) error {
+	userCookie, _:= ctx.Cookie("user")
+	return ctx.JSON(http.StatusAccepted, fmt.Sprintf("You have been authenticated %s", userCookie.Value ))
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,19 @@ module gamma
 go 1.17
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/lib/pq v1.10.3
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 )
 
-require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+require (
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
+	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)

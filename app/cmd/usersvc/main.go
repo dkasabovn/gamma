b06264a251b01248305e50ccad5996218a277c84
dkasabovn <dkasabovn@gmail.com>
2021-10-20 18:20:11 -0500

    fixed go.mod

--- app/cmd/usersvc/main.go
+++ app/cmd/usersvc/main.go
@@ -5,34 +5,17 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-<<<<<<< HEAD
-	"github.com/labstack/echo/v4/middleware"
-=======
->>>>>>> 365cd9b424e878144faca027c882549217cb0266
 )
 
 func main() {
 
 	e := echo.New()
 
-<<<<<<< HEAD
-	authRequired := e.Group("/api")
-
-	// Configure middleware with the custom claims type
-	jwtConfig := middleware.JWTConfig{
-		Claims:      &auth.Claims{},
-		SigningKey:  []byte(auth.GetJwtSecret()),
-		TokenLookup: "cookie:access-token",
-	}
-	authRequired.Use(middleware.JWTWithConfig(jwtConfig))
-	authRequired.GET("", authNeed)
-=======
 	//any request should try to update the tokens
 	e.Use(user.MiddleTokenUpdate)
 
 	// adds temp get and post routes
 	user.JwtRoutes(e)
->>>>>>> 365cd9b424e878144faca027c882549217cb0266
 
 	// temp no auth rout
 	e.GET("/", noAuth)
@@ -43,13 +26,4 @@ func main() {
 
 func noAuth(ctx echo.Context) error {
 	return ctx.JSON(http.StatusAccepted, "No auth needed")
-<<<<<<< HEAD
 }
-
-func authNeed(ctx echo.Context) error {
-	userCookie, _ := ctx.Cookie("user")
-	return ctx.JSON(http.StatusAccepted, fmt.Sprintf("You have been authenticated %s", userCookie.Value))
-}
-=======
-}
->>>>>>> 365cd9b424e878144faca027c882549217cb0266
--- go.mod
+++ go.mod
@@ -3,11 +3,7 @@ module gamma
 go 1.17
 
 require (
-<<<<<<< HEAD
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-=======
 	github.com/golang-jwt/jwt v3.2.2+incompatible
->>>>>>> 365cd9b424e878144faca027c882549217cb0266
 	github.com/labstack/echo/v4 v4.6.1
 	github.com/lib/pq v1.10.3
 	github.com/magefile/mage v1.11.0
@@ -15,7 +11,6 @@ require (
 )
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -24,9 +19,5 @@ require (
 	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
 	golang.org/x/sys v0.0.0-20211020174200-9d6173849985 // indirect
 	golang.org/x/text v0.3.7 // indirect
-<<<<<<< HEAD
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
-=======
-	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
->>>>>>> 365cd9b424e878144faca027c882549217cb0266
 )

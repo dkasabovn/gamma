d39c25319601e6122c87cded7e9088800995a476
dkasabovn <dkasabovn@gmail.com>
2022-01-18 17:23:41 -0600

    remove more college student-esk test data

--- db/event/populate.sql
+++ db/event/populate.sql
@@ -3,7 +3,7 @@ INSERT INTO users (uuid, email, first_name, last_name) VALUES ('dummy', 'dummy@d
 
 INSERT INTO organizations (org_name, city) VALUES ('Big Brewery', 'Homieville');
 
-INSERT INTO events (event_name, event_location, event_date, organization_fk, uuid) VALUES ('White Claw Sampling', 'Mommy Milkers University', '2017-03-14', 1, 'test');
+INSERT INTO events (event_name, event_location, event_date, organization_fk, uuid) VALUES ('White Claw Sampling', 'Big Man University', '2017-03-14', 1, 'test');
 
 INSERT INTO user_events (user_fk, event_fk) VALUES (1, 1);
 

d6ab020e1ff620063d2a0ef01bb5f470ab26da57
gabrieldiaziv <gabrieldiaziv@gmail.com>
2022-03-29 14:40:40 -0500

    testing and user enpoint

--- app/api/user/user_controller.go
+++ app/api/user/user_controller.go
@@ -1,1 +1,28 @@
-package user
+package user_api
+
+import (
+	"gamma/app/api/core"
+	"gamma/app/services/user"
+	"gamma/app/system/auth/ecJwt"
+	"net/http"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+)
+
+func GetUserController(c echo.Context) error {
+	token := c.Get("user").(*jwt.Token)
+	claims := token.Claims.(*ecJwt.GammaClaims)
+	uuid := claims.Uuid
+
+	user, err := user.GetUserService().GetUser(c.Request().Context(), uuid)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, core.ApiError(http.StatusBadRequest))
+	}
+
+	return c.JSON(http.StatusAccepted, core.ApiSuccess(map[string]interface{}{
+		"user" : user,
+	}))
+
+
+}
